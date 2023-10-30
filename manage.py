ba432ebeb73fb5d1c44d2184c8dedcecbf4def66
Dantech <chukwujidan@gmail.com>
2023-10-30 19:55:13 +0100

    new commit

--- manage.py
+++ manage.py
@@ -3,13 +3,13 @@
 import os
 import sys
 from dotenv import load_dotenv
-
+from decouple import config
 load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paylio.settings_pro' if os.environ.get('DJANGO_ENVIRONMENT') == 'production' else 'paylio.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paylio.settings_pro' if config('DJANGO_ENVIRONMENT') == 'production' else 'paylio.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

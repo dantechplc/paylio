42f082d5ec03e284646928fd93aaf9d943ce44e3
Dantech <chukwujidan@gmail.com>
2024-10-20 10:06:54 +0100

    fixed signup bug

--- paylio/settings_pro.py
+++ paylio/settings_pro.py
@@ -59,9 +59,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.fineasebank.com",
 ]
 
-SECURE_SSL_REDIRECT = True
-USE_X_FORWARDED_HOST = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 ROOT_URLCONF = 'paylio.urls'
 

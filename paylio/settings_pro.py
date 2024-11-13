0be7225556ee59e86814a85a7ceb13f35c4ad85f
Dantech <chukwujidan@gmail.com>
2024-11-13 23:34:21 +0100

    added the domain name

--- paylio/settings_pro.py
+++ paylio/settings_pro.py
@@ -51,10 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    "https://grandsinspc.com",
-    "https://www.grandsinspc.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://grandsinspc.com",
+#     "https://www.grandsinspc.com",
+# ]
 # CSRF_TRUSTED_ORIGINS = [
 #     "https://grandsinspccom",
 #     "https://www.grandsinspc.com",

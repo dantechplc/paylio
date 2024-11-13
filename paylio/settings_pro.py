52a8cc4668bc6753ae7371435769a5835902b022
Dantech <chukwujidan@gmail.com>
2024-11-13 23:26:13 +0100

    added the domain name

--- paylio/settings_pro.py
+++ paylio/settings_pro.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4%twcf7_z9hgx2ka^(41%-!!c9z0k3!$u#bo9sb=ksmrdfpqh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["51.20.103.182", "grandsinspc.com", "www.grandsinspc.com"]
 
 # Application definition
